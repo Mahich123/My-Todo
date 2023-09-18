fa52c8fe6d53ff211355f20302b6bf7903e94050
Mahi <mahichowdhury587@gmail.com>
2023-09-19 02:17:20 +0600

    fixed functional codes

--- updateFunctional.js
+++ updateFunctional.js
@@ -1,160 +1,173 @@
 // LOCAL STORAGE
 
-const localStore = (key,data) => {
-    if(data !== undefined) {
-        localStorage.setItem(key, JSON.stringify(data))
-    }
-    let storedData = localStorage.getItem(key);
-    return storedData ?  JSON.parse((storedData)) : [];
-}
-
-// DOM PART
-
-const getInput = (inputId) => {
-  return document.getElementById(inputId)
+const localStore = (key, data) => {
+  if(data !== undefined) {
+      localStorage.setItem(key, JSON.stringify(data))
+  }
+  let storedData = localStorage.getItem(key)
+  return storedData ? JSON.parse(storedData) : []
 }
 
 const getMultiple = (inputId) => {
   return document.querySelectorAll(inputId)
 }
 
-const formEvent = (input, newvalue) => {
+const getInput = (inputId) => {
+  return document.getElementById(inputId)
+}
+
+const formValue = (input) => {
+  return input.value
+}
+
+
+
+const formEvent = (input, callback) => {
   input.addEventListener("keydown", (e) => {
       if(e.key === "Enter") {
-          newvalue(input.value)
+          callback()
       }
   })
 }
 
-const modalEvent = (data, update) => {
-  data.addEventListener("keydown", (e) => {
-      if(e.key === "Enter") {
-          update(data.value)
-          modal.style.display = "none"
-      }
+
+const formSubmit = () => {
+  const input = getInput("inputData")
+  formEvent(input, () => {
+    const prevItem = Array.from(localStore('items'))
+    const newItem = formValue(input)
+    prevItem.unshift(newItem)
+    localStore("items", prevItem )
+    input.value = ""
+    updateData()
   })
 }
-
-const modalClose = (back, modal) => {
-  back.addEventListener("click", () => {
-      modal.style.display = "none"
+ 
+const displayContent = () => {
+  // const lists = getInput("listed")
+  let storedData = localStore("items").map((i) => {
+      return(
+          `
+          <div class="p-8  md:mt-0 py-6  border-r-4  shadow-lg border-l-4 border-[#4b8b6d] rounded w-2/3 md:w-1/4 flex  justify-between items-center">
+          <div class="cursor-pointer popup-input break-anywhere ">${i}</div>
+         <div class="flex items-center gap-2">
+         <div id="dust">
+           <i class="fa-solid fa-trash cursor-pointer "></i>
+         </div>
+           <i id="edit" class="fa-solid fa-pen-to-square cursor-pointer"></i>
+         </div>
+        </div>
+          `
+      )
   })
+
+  return storedData.join('')
 }
 
-
-// SIDE EFFECTS
-
-const formInput = () => {
-    const todoInput = getInput("inputData")
-    formEvent(todoInput, (t) => {
-        const prevItem = Array.from(localStore("items"))
-        prevItem.unshift(t)
-        localStore("items", prevItem)
-        
-        displayData()
-
-        todoInput.value = ""
-    })
-}
 
 const displayData = () => {
-    const lists = getInput("listed")
-    let storedData = localStore("items").map((i) => { 
-        return(
-            `
-            <div class="p-8  md:mt-0 py-6  border-r-4  shadow-lg border-l-4 border-[#4b8b6d] rounded w-2/3 md:w-1/4 flex  justify-between items-center">
-            <div class="cursor-pointer popup-input break-anywhere ">${i}</div>
-           <div class="flex items-center gap-2">
-           <div id="dust">
-             <i class="fa-solid fa-trash cursor-pointer "></i>
-           </div>
-             <i id="edit" class="fa-solid fa-pen-to-square cursor-pointer"></i>
-           </div>
-          </div>
-            `
-        )
-    })
-
-    lists.innerHTML = storedData.join('')
-    removeData()
-    editTodo()
+  const lists = getInput("listed")
+  const storedTodo = displayContent()
+  lists.innerHTML = storedTodo 
+  removeData()
+  editTodo()
 }
 
+
+const remove = (i) => {
+  const prevData = localStore("items")
+  prevData.splice(i,1)
+  localStore("items", prevData)
+  updateData()
+}
 
 const removeData = () => {
-    const remove = getMultiple("#dust")
-    
-    Array.from(remove).map((e,i) => {
-        e.addEventListener("click", () => {
-          const presentData = localStore("items")
-          presentData.splice(i,1)
-          localStore("items", presentData)
-          location.reload()
-        })
-    }) 
-}
-
-
-
-const updateValue = (text) => {
-  let up = getInput("up")
-  modalEvent(up, () => {
-    let todoData = localStore("items")
-    const index = localStore("items").indexOf(text)
-    todoData[index] = up.value
-    localStore("items", todoData)
-    displayData()
-    modalOff()
+  const dustbtn = getMultiple("#dust")
+  Array.from(dustbtn).map((e,i) => {
+      e.addEventListener("click", () => {
+          remove(i)
+      })
   })
 }
 
-const modalOff = () => {
-    const modal = getInput("modal")
-    let back = getInput("back")
-    modalClose(back, modal)
+const updateData = () => {
+  const localData = localStore("items")
+  displayData(localData)
 }
 
-const modalOpen = (modal, content) => {
-    modal.innerHTML = ``;
-    modal.insertAdjacentHTML('beforeend', content);
-    modal.style.display = "flex"
-    modalOff()
+const modalData = (index,newval) => {
+  let todoData = localStore("items")
+  todoData[index] = newval
+  localStore("items", todoData)
+  return todoData
 }
 
-const editHandler = (edit) => {
-     const todotText = edit.parentNode.parentNode.querySelector(".popup-input").textContent;
-     const modal = getInput("modal")
+const enterEvent = (text) => {
+  let up = getInput("up")
+  up.addEventListener("keydown", (e) => {
+      if(e.key === "Enter") {
+          const index = localStore("items").indexOf(text)
+          const updateTodo = modalData(index, up.value)   
+          localStore("items", updateTodo)
+          modalClose(getInput("modal")) // Close the modal
+          updateData()
+      }
+  })
+}
 
-     const content = `
-     <p class="w-full text-xs text-left">This is your editspace</p>
-      <div>
-         <input id="up" type="text" class="text-sm mt-2 outline-none bg-transparent" value="${todotText}">
-     </div>
-     <div id="back" class="absolute top-0 right-0 cursor-pointer"><i class="fa-solid fa-arrow-left"></i></div>
+
+const clickEvent = () => {
+  const modal = getInput('modal')
+  let back = getInput("back")
+  back.addEventListener("click" , () => {
+      modalClose(modal)
+  })
+}
+
+
+const modalContent = (text) => {
+  return `
+      <p class="w-full text-xs text-left">This is your editspace</p>
+          <div>
+          <input id="up" type="text" class="text-sm mt-2 outline-none bg-transparent" value="${text}">
+          </div>
+          <div id="back" class="absolute top-0 right-0 cursor-pointer"><i class="fa-solid fa-arrow-left"></i></div>
   `;
-
-     modalOpen(modal, content)
-   
-     updateValue(todotText)
-     
 }
-
 
 const editTodo = () => {
-    const editBtns = getMultiple("#edit");
-    Array.from(editBtns).map((edit) => {
-        edit.addEventListener("click", () => {
-            editHandler(edit);      
-        });
-    });
-};
+ const modal = getInput("modal")
+ const edit = getMultiple("#edit")
+  Array.from(edit).map((e) => {
+      e.addEventListener("click", () => {
+          let todotText = e.parentNode.parentNode.querySelector(".popup-input").textContent;
+          const modalText = modalContent(todotText)
+          modalOpen(modal,modalText)
+          enterEvent(todotText)
+      })
+  })
+}
 
 
+const modalOpen = (modal, content) => {
+  modal.innerHTML = content
+  modal.style.display = "flex"
+  clickEvent()
+}
 
 
-formInput()
-displayData()
-removeData()
-localStore("items")
+const modalClose = (modal) => {
+  if (modal) {
+      modal.style.display = "none";
+  }
+}
+
+
+displayData();
+formSubmit();
+removeData();
+editTodo();
+localStore("items");    
+
 
 
