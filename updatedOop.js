5d0b33711953fd07dbfbdf61fd64080c67547b8e
Mahi <mahichowdhury587@gmail.com>
2023-09-19 22:08:31 +0600

    Updated oop code

--- updatedOop.js
+++ updatedOop.js
@@ -10,16 +10,150 @@ class LocallyStored {
 }
 
 
-class DomElements {
+
+
+
+class TodoData {
+    constructor() {
+        this.todoArray = []
+    }
+
+    // create
+    addTodo(todo) {
+        this.todoArray.unshift(todo)
+    }  
+
+    // read
+    read() {
+        return this.todoArray
+    }
+
+    
+    // update
+    edit(oldValue, newValue) {
+           const todoIndex = this.todoArray.indexOf(oldValue)
+           if(todoIndex !== -1) {
+                this.todoArray[todoIndex] = newValue    
+           }
+    }
+
+    // delete
+    deleteTodo(todo) {
+        const todoIndex = this.todoArray.indexOf(todo)
+        if(todoIndex !== -1) {
+           this.todoArray.splice(todoIndex,1)
+        }
+    }
+
+    // list
+    list() {
+        this.read()
+    }
+    
+}
+
+
+
+
+
+class TodoUI {
     constructor() {
         this.input = document.querySelector("#inputData")
-        this.modal = document.querySelector("#modal")
-        this.todoData = new TodoData()
+        this.modal = document.querySelector("#modal")   
+        this.lists = document.querySelector("#listed")
+        this.localdata = new LocallyStored();
+        this.todoData = new TodoData();
+        this.todoData.todoArray = this.localdata.getData("items") || [];
         this.input.addEventListener("keydown", (e) => {
-            todoUi.AddingTodo(e)
+            this.AddingTodo(e)
         })
         this.oldValue = ""
+        this.DisplayTodo();
+    }
+
+  
+
+    AddingTodo(e) {
+        if(e.key === "Enter") {
+          const input = this.Input() 
+          this.todoData.addTodo(input)
+          const data = this.todoData.todoArray
+          this.localdata.StoreData("items", data)
+          this.clearInput()
+          this.DisplayTodo()
+        }
+    }
+
+    DisplayTodo() {
+         let lists = this.lists
+
+         if(Array.isArray(this.todoData.todoArray)) {
+            let storedItem = this.todoData.todoArray.map((i) => {
+                return(
+                    `
+                    <div class="p-8  md:mt-0 py-6  border-r-4  shadow-lg border-l-4 border-[#4b8b6d] rounded w-2/3 md:w-1/4 flex  justify-between items-center">
+                    <div class="cursor-pointer popup-input break-anywhere ">${i}</div>
+                    <div class="flex items-center gap-2">
+                    <div id="dust">
+                        <i class="fa-solid fa-trash cursor-pointer "></i>
+                    </div>
+                        <i class="edit fa-solid fa-pen-to-square cursor-pointer"></i>
+                    </div>
+                    </div>
+                    `
+                )
+            });
+
+            lists.innerHTML = storedItem.join('');
+           
+            this.EditListener();
+
+            this.deleteTodo().forEach((d,i) => {
+                d.addEventListener("click", () => {
+                    this.DeleteTodo(i)
+                })
+            }) 
+        }
+    }
+
+    EditTodo(i, todoArray) {
+    //   console.log("todoArray:", todoArray)
+        let itemText = this.todoData.todoArray[i]
+        // console.log("itemText:", itemText)
+        const content = `
+             <p class="w-full text-xs text-left">This is your editspace</p>
+                <div>
+                    <input id="up" type="text" class="text-sm mt-2 outline-none bg-transparent" value="${itemText}">
+                </div>
+            <div id="back" class="absolute top-0 right-0 cursor-pointer"><i class="fa-solid fa-arrow-left"></i></div>            
+        `
+
+        this.SetModal(content)
+        this.displayModal()
+
+        this.back()
+
+        let up = this.Editinput()
+        up.addEventListener("keydown", (e) => {
+            if(e.key === "Enter") {
+                const editValue = this.EditText()
+                // console.log("ediValue:",editValue)
+                this.todoData.edit(itemText, editValue)
+                this.CloseModal()
+                this.DisplayTodo()
+            }
+        })
+
         
+             
+    }
+
+    DeleteTodo(index) {
+        this.todoData.deleteTodo(this.todoData.todoArray[index]);
+        const data = this.todoData.todoArray;
+        this.clearInput();
+        this.DisplayTodo();
+        this.localdata.StoreData("items", data);
     }
 
     Input() {
@@ -34,9 +168,7 @@ class DomElements {
 
         pens.forEach((pen, index) => {
             pen.addEventListener("click", () => {
-            const todoArr = this.todoData.todoArray
-            this.oldValue = todoArr[index]
-            todoUi.EditTodo(index, todoArr)
+            this.EditTodo(index, this.todoData.todoArray);
                
             });
         });
@@ -83,141 +215,6 @@ class DomElements {
     EditText() {
         return this.Editinput().value
     }
-
-    
-}
-
-
-
-class TodoData {
-    constructor() {
-        // this.todoData = new DomElements()
-        this.localdata = new LocallyStored()
-        this.todoArray = []
-    }
-
-    addTodo(todo) {
-        this.todoArray.unshift(todo)
-    }  
-    
-
-
-
-    editTodo(oldValue, newValue) {
-           const todoIndex = this.todoArray.indexOf(oldValue)
-           if(todoIndex !== -1) {
-                this.todoArray[todoIndex] = newValue    
-           }
-       }
-
-    deleteTodo(todo) {
-        const todoIndex = this.todoArray.indexOf(todo)
-        if(todoIndex !== -1) {
-           this.todoArray.splice(todoIndex,1)
-        }
-    }
-
-    
-}
-
-
-
-
-
-class TodoUI {
-    constructor() {
-        this.inputDom = new DomElements();
-        this.localdata = new LocallyStored();
-        this.todoData = new TodoData();
-        this.todoData.todoArray = this.localdata.getData("items") || [];
-        this.DisplayTodo();
-    }
-
-  
-
-    AddingTodo(e) {
-        if(e.key === "Enter") {
-          const input = this.inputDom.Input() 
-          this.todoData.addTodo(input)
-          const data = this.todoData.todoArray
-          this.localdata.StoreData("items", data)
-          this.inputDom.clearInput()
-          this.DisplayTodo()
-        }
-    }
-
-    DisplayTodo() {
-         let lists = this.inputDom.todoLists()
-
-         if(Array.isArray(this.todoData.todoArray)) {
-            let storedItem = this.todoData.todoArray.map((i) => {
-                return(
-                    `
-                    <div class="p-8  md:mt-0 py-6  border-r-4  shadow-lg border-l-4 border-[#4b8b6d] rounded w-2/3 md:w-1/4 flex  justify-between items-center">
-                    <div class="cursor-pointer popup-input break-anywhere ">${i}</div>
-                    <div class="flex items-center gap-2">
-                    <div id="dust">
-                        <i class="fa-solid fa-trash cursor-pointer "></i>
-                    </div>
-                        <i class="edit fa-solid fa-pen-to-square cursor-pointer"></i>
-                    </div>
-                    </div>
-                    `
-                )
-            });
-
-            lists.innerHTML = storedItem.join('');
-           
-            this.inputDom.EditListener();
-
-            this.inputDom.deleteTodo().forEach((d,i) => {
-                d.addEventListener("click", () => {
-                    this.DeleteTodo(i)
-                })
-            }) 
-        }
-    }
-
-    EditTodo(i, todoArray) {
-      console.log("todoArray:", todoArray)
-        let itemText = todoArray[i]
-        console.log("itemText:", itemText)
-        const content = `
-             <p class="w-full text-xs text-left">This is your editspace</p>
-                <div>
-                    <input id="up" type="text" class="text-sm mt-2 outline-none bg-transparent" value="${itemText}">
-                </div>
-            <div id="back" class="absolute top-0 right-0 cursor-pointer"><i class="fa-solid fa-arrow-left"></i></div>            
-        `
-
-        this.inputDom.SetModal(content)
-        this.inputDom.displayModal()
-
-        this.inputDom.back()
-
-        let up = this.inputDom.Editinput()
-        up.addEventListener("keydown", (e) => {
-            if(e.key === "Enter") {
-                const editValue = this.inputDom.EditText()
-                console.log("ediValue:",editValue)
-                this.todoData.editTodo(itemText, editValue)
-                this.inputDom.CloseModal()
-                this.DisplayTodo()
-            }
-        })
-
-        
-             
-    }
-
-    DeleteTodo(index) {
-        this.todoData.deleteTodo(this.todoData.todoArray[index]);
-        const data = this.todoData.todoArray;
-        this.inputDom.clearInput();
-        this.DisplayTodo();
-        this.localdata.StoreData("items", data);
-    }
-
 
  
 }
